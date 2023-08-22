182d4228a5d7cf529cb0f9c9b7a8d2e2dd037b2b
Ioramka <ana.ioramashvili.1@btu.edu.ge>
2023-08-22 22:14:36 +0400

    changed and simplified version of the app

--- ChuckNorrisApp/ViewController.swift
+++ ChuckNorrisApp/ViewController.swift
@@ -1,52 +1,116 @@
 import UIKit
 
-// Define the Chuck Norris Joke structure
 struct ChuckNorrisJoke: Codable {
     let value: String
 }
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet weak var jokeLabel: UILabel!
     @IBOutlet weak var jokeButton: UIButton!
-    
+    @IBOutlet weak var categoryPicker: UIPickerView!
+
+    var categories: [String] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        jokeLabel.text = ""
-        jokeLabel.center = view.center
-        jokeButton.setTitle("Generate", for: .normal)
+        setupUI()
+        fetchCategories()
+    }
+
+    func setupUI() {
+        // Set up the label
         jokeLabel.numberOfLines = 0
-        
-        jokeButton.titleLabel?.font = UIFont.systemFont(ofSize: 16)
-        jokeButton.backgroundColor = UIColor.blue
+        jokeLabel.textAlignment = .center
+        jokeLabel.text = ""
+
+        // Set up the joke button
+        jokeButton.layer.cornerRadius = 8
+        jokeButton.backgroundColor = UIColor.systemBlue
         jokeButton.setTitleColor(UIColor.white, for: .normal)
-        jokeButton.layer.cornerRadius = 10
-        jokeButton.layer.borderColor = UIColor.black.cgColor
-        jokeButton.layer.borderWidth = 1
-        
+        jokeButton.setTitle("Generate", for: .normal)
         
 
+        // Set up the category picker
+        categoryPicker.delegate = self
+        categoryPicker.dataSource = self
+    }
 
+    func fetchCategories() {
+        guard let categoriesUrl = URL(string: "https://api.chucknorris.io/jokes/categories") else {
+            return
+        }
 
-
+        URLSession.shared.dataTask(with: categoriesUrl) { data, response, error in
+            if let data = data {
+                do {
+                    // Decode the categories from the data
+                    let categoriesResponse = try JSONDecoder().decode([String].self, from: data)
+                    DispatchQueue.main.async {
+                        // Update the categories and reload the picker
+                        self.categories = categoriesResponse
+                        self.categoryPicker.reloadAllComponents()
+                    }
+                } catch {
+                    print("Error decoding categories: \(error)")
+                }
+            }
+        }.resume()
     }
 
     @IBAction func fetchJoke(_ sender: UIButton) {
-        // Define the Chuck Norris API URL
-        let apiUrl = URL(string: "https://api.chucknorris.io/jokes/random")!
+        // Get the selected category from the picker
+        let selectedCategory = categories[safe: categoryPicker.selectedRow(inComponent: 0)] ?? ""
 
-        // Fetch the Chuck Norris joke
-        if let data = try? Data(contentsOf: apiUrl),
-           let joke = try? JSONDecoder().decode(ChuckNorrisJoke.self, from: data) {
-            
-            // Fade in the joke label with animation
-            UIView.animate(withDuration: 0.5, animations: {
-                self.jokeLabel.alpha = 0  // Start with zero opacity
-                self.jokeLabel.text = joke.value
-                self.jokeLabel.alpha = 1  // Fade in to full opacity
-            })
-        } else {
-            jokeLabel.text = "Failed to fetch or parse Chuck Norris joke."
+        // Build the URL for fetching a random joke from the selected category
+        let apiUrlString = "https://api.chucknorris.io/jokes/random?category=\(selectedCategory)"
+        guard let apiUrl = URL(string: apiUrlString) else {
+            return
         }
+
+        URLSession.shared.dataTask(with: apiUrl) { data, response, error in
+            if let data = data {
+                do {
+                    // Decode the joke from the data
+                    let joke = try JSONDecoder().decode(ChuckNorrisJoke.self, from: data)
+                    DispatchQueue.main.async {
+                        // Display the joke with a fade-in animation
+                        UIView.animate(withDuration: 0.5) {
+                            self.jokeLabel.alpha = 0
+                            self.jokeLabel.text = joke.value
+                            self.jokeLabel.alpha = 1
+                        }
+                    }
+                } catch {
+                    DispatchQueue.main.async {
+                        self.jokeLabel.text = "Failed to fetch or parse Chuck Norris joke."
+                    }
+                }
+            }
+        }.resume()
+    }
+}
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categories.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categories[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // You can add further actions here if needed
+    }
+}
+
+extension Collection {
+    subscript(safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
     }
 }
